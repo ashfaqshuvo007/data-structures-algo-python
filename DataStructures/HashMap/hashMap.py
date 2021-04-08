ba7a3831435203c3559045eb412e4a7f32d1e0f9
Ashfaq H Ahmed <ashfaqshuvo14@gmail.com>
2021-04-08 18:19:52 +0600

    Opened in pyCharm

--- DataStructures/HashMap/hashMap.py
+++ DataStructures/HashMap/hashMap.py
@@ -1,13 +1,13 @@
 # hashMap is used behind the scenes while using dictionary in python
-#lets see how it implements hashmap
+# lets see how it implements hashmap
 
 class HashMap:
     def __init__(self):
         self.MAX = 100
         self.arr = [None for i in range(self.MAX)]
 
-    #Hash function to get location from Ram
-    def get_hash(self,key):
+    # Hash function to get location from Ram
+    def get_hash(self, key):
         h = 0
         for char in key:
             h += ord(char)
@@ -17,7 +17,7 @@ class HashMap:
     #     h = self.get_hash(key)
     #     self.arr[h] = value
 
-    #python builtin function replaces add to allow dictionary sysntaxx
+    # python builtin function replaces add to allow dictionary syntax
     def __setitem__(self, key, value):
         h = self.get_hash(key)
         self.arr[h] = value
@@ -26,20 +26,21 @@ class HashMap:
     #     h = self.get_hash(key)
     #     return self.arr[h]
 
-    #python builtin function replaces add to allow dictionary sysntaxx
+    # python builtin function replaces add to allow dictionary syntax
     def __getitem__(self, key):
         h = self.get_hash(key)
         return self.arr[h]
 
-    #Similarly
+    # Similarly
     def __delitem__(self, key):
         h = self.get_hash(key)
         self.arr[h] = None
 
+
 if __name__ == '__main__':
     hm = HashMap()
     print(hm.arr)
-    hm['june 6'] =  130
+    hm['june 6'] = 130
     hm['june 28'] = 1110
     hm['june 8'] = 10
     hm['june 18'] = 101
@@ -48,6 +49,3 @@ if __name__ == '__main__':
     print(hm.arr)
     del hm['aug 23']
     print(hm.arr)
-
-
-    
