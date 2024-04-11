9c9d57065822e24d4ab64d4faabafbde60f8bf15
Ashfaq Hussain Ahmed <ashfaqshuvo14@gmail.com>
2024-04-11 12:52:04 +0300

    Leetcode problems: 1, 49

---
+++ leetcode/1-TwoSum.py
@@ -0,0 +1,18 @@
+"""
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+You can return the answer in any order.
+"""
+nums = [2,7,11,15]
+target = 9
+def twoSum(nums, target):
+    prevMap = {}
+    for i,n in enumerate(nums):
+        diff = target - n
+        if diff in prevMap:
+            return [prevMap[diff], i]
+        prevMap[n] = i
+    return
+        
---
+++ leetcode/49-GroupAnagrams.py
@@ -0,0 +1,20 @@
+"""
+Given an array of strings 'strs', group the anagrams together. You can return the answer in any order.
+
+An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+
+"""
+from collections import defaultdict
+strs = ["eat","tea","tan","ate","nat","bat"]
+
+def groupAnagrams(strs):
+    anagrams = defaultdict(list)
+    for word in strs:
+        sortedWord = ''.join(sorted(word))
+        if  sortedWord not in anagrams:
+            anagrams[sortedWord] = [word]
+        else: 
+            anagrams[sortedWord].append(word)
+    print(list(anagrams.values()))
+            
+            
