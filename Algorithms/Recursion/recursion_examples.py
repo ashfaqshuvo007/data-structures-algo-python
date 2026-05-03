0588bf87c13fc876dbd5f97bdc100a96f0676c1c
Ashfaq Hussain Ahmed <ashfaqshuvo14@gmail.com>
2026-05-03 08:05:03 +0600

    Some basic recursion examples explored. Revisited Leetcode 36 - Valid Sudoku

--- Algorithms/Recursion/recursion_examples.py
+++ Algorithms/Recursion/recursion_examples.py
@@ -1,2 +1,53 @@
 
 #* Recusion Basic examples:
+
+#? Recursively calculate the sum of list of numbers
+from typing import List
+class Recursion:
+    def recursive_sum(self, nums: List[int]) -> int: 
+        if not nums:
+            return 0
+        return nums[0] + self.recursive_sum(nums[1:])
+
+    def count_items(self, arr: List[any]) -> int:
+        if not arr:
+            return 0
+        return 1 + self.count_items(arr[1:])
+
+    def max_num(self, nums: List[int]) -> int:
+        if not nums:
+            return -1
+        maxNum = nums[0]
+        return max(maxNum, self.max_num(nums[1:]))
+
+    def binary_search(self, nums: List[int], l: int, r: int, target: int) -> bool:
+        m = (l + r) // 2
+
+        if nums[m] == target:
+            return True
+
+        if  target < nums[m]:
+            return self.binary_search(nums,l, m - 1, target)
+        if target > nums[m]:
+            return self.binary_search(nums,m + 1, r, target)
+
+
+if __name__ == "__main__":
+    numsList = [10, 20, 30, 40, 50, 60]
+    recursion = Recursion()
+
+    #* Recursively calculate the sum of array elements
+    # sum_recursive = recursion.recursive_sum(numsList)
+    # print(sum_recursive)
+
+    #* Recursively calculate the number of elements in an array
+    # mixedList = [10, 20, "30", 40, "50", 60]
+    # count_items = recursion.count_items(mixedList)
+    # print(count_items)
+
+    #* Recursively calculate the max of array elements
+    # max_num = recursion.max_num(numsList)
+    # print(max_num)
+
+    #* Recursive Binary Search
+    print(recursion.binary_search(numsList,0, len(numsList) -1, 60))
--- leetcode/Array&Hashing/36-ValidSudoku.py
+++ leetcode/Array&Hashing/36-ValidSudoku.py
@@ -12,6 +12,22 @@ Note:
 
 '''
 
+#* Hash Set (One Pass) - Intuition
+
+# * Instead of checking rows, columns, and 3×3 boxes separately, we can validate the entire Sudoku board in one single pass.
+
+# * For each cell, we check whether the digit has already appeared in:
+# *   - the same row
+# *   - the same column
+# *   - the same 3×3 box
+
+# * We track these using three hash sets:
+
+# * rows[r] keeps digits seen in row r
+# * cols[c] keeps digits seen in column c
+# * squares[(r // 3, c // 3)] keeps digits in the 3×3 box
+# * If a digit appears again in any of these places, the board is invalid.
+
 from collections import defaultdict
 from typing import List
 class Solution:
@@ -28,7 +44,7 @@ class Solution:
                 # the current positon value is already visited
                 if (board[r][c] in rows[r] or
                     board[r][c] in cols[c] or
-                    board[r][c] in squares[(r // 3, c // 3)]):
+                    board[r][c] in squares[(r // 3, c // 3)]): # this check is where the magic happens
                     return False
                 # Add current value to matrix if not seen
                 cols[c].add(board[r][c])
