7f3a9b49b994fdb1cd0bcf008f1acefcf9031a6b
Ashfaq Hussain Ahmed <ashfaqshuvo14@gmail.com>
2026-06-05 08:03:10 +0600

    Solved Heap(Priority Queue) Problems - 295. Find Median from Data Stream - HARD

---
+++ leetcode/Heap(PriorityQueue)/295-FindMedianDataStream.py
@@ -0,0 +1,55 @@
+
+#* 295 - Find Median from Data Stream - HARD
+
+'''
+The median is the middle value in a sorted list of integers. For lists of even length, 
+there is no middle value, so the median is the mean of the two middle values.
+
+For example:
+
+For arr = [1,2,3], the median is 2.
+For arr = [1,2], the median is (1 + 2) / 2 = 1.5
+
+Implement the MedianFinder class:
+
+– MedianFinder() initializes the MedianFinder object.
+- void addNum(int num) adds the integer num from the data stream to the data structure.
+- double findMedian() returns the median of all elements so far.
+
+
+'''
+
+import heapq
+class MedianFinder:
+
+    def __init__(self):
+        # Left or smaller elements. The largest number of this half is on top.
+        self.small = []
+        # Right or larger elements. The smallest number of this half is on top.
+        self.large = []
+
+    def addNum(self, num: int) -> None:
+        if self.large and num > self.large[0]:
+            heapq.heappush(self.large, num)
+        else:
+            heapq.heappush(self.small, -1 * num)
+
+        if len(self.small) > len(self.large) + 1:
+            val = -1 * heapq.heappop(self.small)
+            heapq.heappush(self.large, val)
+        if len(self.large) > len(self.small) + 1:
+            val = heapq.heappop(self.large)
+            heapq.heappush(self.small, -1 * val)
+
+    def findMedian(self) -> float:
+        if len(self.small) > len(self.large):
+            return -1 * self.small[0]
+        elif len(self.large) > len(self.small):
+            return self.large[0]
+        return (-1 * self.small[0] + self.large[0]) / 2.0
+
+
+# Your MedianFinder object will be instantiated and called as such:
+# obj = MedianFinder()
+# obj.addNum(num)
+# param_2 = obj.findMedian()
